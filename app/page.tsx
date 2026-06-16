926a62b0f5a12e62357f4a2fd2c660d72ad23e9e
MicTalks <mictalkscorporative@gmail.com>
2026-06-16 00:02:34 -0300

    known-tech page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "./components/pages/home/hero-section";
+import { KnownTechs } from "./components/pages/known-techs";
 
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <KnownTechs />
     </>
   )
 }
