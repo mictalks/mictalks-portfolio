cf9cf109481e3715c46d6341237f22b91cf5e27c
MicTalks <mictalkscorporative@gmail.com>
2026-07-14 23:03:06 -0300

    fix: validate hygraph config and make token optional

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { Inter, IBM_Plex_Mono } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
 import { Header } from './components/header'
@@ -17,20 +16,9 @@ export const metadata: Metadata = {
   },
 }
 
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-})
-
-const plexMono = IBM_Plex_Mono({
-  variable: '--font-plex-mono',
-  subsets: ['latin'],
-  weight: ['400', '500'],
-})
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
+    <html lang="pt-BR">
       <body>
         <Header />
         {children}
